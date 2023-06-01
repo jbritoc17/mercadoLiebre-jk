1bb380614896a2de29ebb0baaad2aefc039afcc6
jbritoc17 <jbritoc17@outlook.es>
2023-06-01 15:28:46 -0500

    modificando archios e instalando method override

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const mainRoutes = require('./routes/mainRoutes');
-const userRoutes = require('./routes/userRoutes');
+const methodOverride = require("method-override");
 
-let app = express();
+const mainRoutes = require("./routes/mainRoutes");
+const userRoutes = require("./routes/userRoutes");
 
-app.set('view engine', 'ejs');
-app.use(express.static("public"));
+const app = express();
 
-app.set('views',[
-    path.join(__dirname, './views/main'),
-    path.join(__dirname, './views/users')
+app.set("view engine", "ejs");
+
+app.set("views", [
+  path.join(__dirname, "./views/main"),
+  path.join(__dirname, "./views/users"),
 ]);
 
+/* --- Middlewares --- */
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride("_method"));
+
+/* --- Routers --- */
 app.use(mainRoutes);
 app.use(userRoutes);
 
-app.listen(PORT, () =>{
-    console.log("servidor Funcionando en el puerto"+ PORT);
+app.listen(PORT, () => {
+  console.log("servidor Funcionando en el puerto http://localhost:" + PORT);
 });
